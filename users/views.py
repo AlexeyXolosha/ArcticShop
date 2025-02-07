d68b7fc92f524985a4bab970e1cf3a1355c3fad5
AlexeyXolosha <alexeyxolosh4@gmail.com>
2025-02-07 17:28:16 +0500

    auth & register

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 
 from users.models import User
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 # Create your views here.
 
@@ -27,14 +27,27 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Вы зарегистрировались')
             return HttpResponseRedirect(reverse('users:login'))
         else:
             print(form.errors)
     else:
-        form = UserRegistrationForm(data=request.POST)
+        form = UserRegistrationForm()
     context = {'form': form}
     return render(request, 'users/registration.html', context)
 
 def profile(request):
-    context = {'title': 'Profile'}
+    if request.method == "POST":
+        form = UserProfileForm(instance=request.user ,data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.errors)
+    form = UserProfileForm(instance=request.user)
+    context = {'title': 'Store - Профиль', 'form': form}
     return render(request, 'users/profile.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
