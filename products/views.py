e0eecb0f2d50a93a415362979f0e31dea627aadb
AlexeyXolosha <alexeyxolosh4@gmail.com>
2025-02-12 11:12:20 +0500

    ProductListView

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 from products.models import ProductCategory, Product, Basket
 from django.core.paginator import Paginator
@@ -15,19 +16,36 @@ class IndexView(TemplateView):
         context['title'] = 'Store'
         return context
 
-def products(request, category_id=None, page_num=1):
-    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+class ProductsListView(ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 2
 
-    per_page = 2
-    paginator = Paginator(products, per_page)
-    products_paginator = paginator.page(page_num)
+    def get_queryset(self):
+        queryset = super(ProductsListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
 
-    context = {
-        'title': 'Store-Каталог',
-        'category': ProductCategory.objects.all(),
-        'products': products_paginator,
-    }
-    return render(request, 'products/products.html', context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductsListView, self).get_context_data(**kwargs)
+        context['title'] = 'Store-Каталог'
+        context['category'] = ProductCategory.objects.all()
+        return context
+
+
+# def products(request, category_id=None, page_num=1):
+#     products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+#
+#     per_page = 2
+#     paginator = Paginator(products, per_page)
+#     products_paginator = paginator.page(page_num)
+#
+#     context = {
+#         'title': '',
+#         'category': ProductCategory.objects.all(),
+#         'products': products_paginator,
+#     }
+#     return render(request, 'products/products.html', context)
 
 
 @login_required
