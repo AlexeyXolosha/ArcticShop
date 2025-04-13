25296d0d910a38f47a41613009b2457466d0b1e3
AlexeyXolosha <AlexeyXolosh4@gmail.com>
2025-04-13 13:20:35 +0500

    rename core

--- Core/urls.py
+++ Core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for djangoProject project.
+URL configuration for Core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -23,6 +23,7 @@ from main.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', IndexView.as_view(), name='index'),
     path('products/', include('products.urls', namespace='products')),
     path('users/', include('users.urls', namespace='users')),
